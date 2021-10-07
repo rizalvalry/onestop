119f2683fff139ed52650cda4a126bce63615c1e
rizalvalry <rizal.muh.rzl@gmail.com>
2021-10-07 10:24:32 +0700

    fix

--- get_skala.php
+++ get_skala.php
@@ -4,12 +4,12 @@
 
 	echo "<option value=''>Pilih Skala</option>";
 
-	$query = "SELECT * FROM skala WHERE id_kelas=? ORDER BY nama ASC";
-	$dewan1 = $conn->prepare($query);
-	$dewan1->bind_param("i", $kelas);
-	$dewan1->execute();
-	$res1 = $dewan1->get_result();
-	while ($row = $res1->fetch_assoc()) {
+	$query = mysqli_query($conn, "SELECT * FROM skala WHERE id_kelas=? ORDER BY nama ASC");
+	// $dewan1 = $conn->prepare($query);
+	// $dewan1->bind_param("i", $kelas);
+	// $dewan1->execute();
+	// $res1 = $dewan1->get_result();
+	while ($row = mysqli_fetch_array($query)) {
 		echo "<option value='" . $row['id_skala'] . "'>" . $row['nama'] . "</option>";
 	}
 ?>
