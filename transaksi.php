e409fc7b46008415bf237d5c16f5e2b8980e850e
rizalvalry <rizal.muh.rzl@gmail.com>
2021-10-07 17:01:24 +0700

    fix

--- transaksi.php
+++ transaksi.php
@@ -130,6 +130,7 @@ if(isset($_SESSION['id'])){
           <a href="#" class="btn btn-primary btn-sm disabled" aria-disabled="true"><span class="glyphicon glyphicon-ok-sign" ></span> Selesai</a>
           <a href="cektransaksi.php?cek=<?php echo $hasil['No_Order']; ?>" class="btn btn-warning btn-sm">Lihat</a>
           <a href="download-laporan.php?cetak=<?php echo $hasil['No_Order']; ?>" class="btn btn-info btn-sm">Cetak</a>
+          <a href="proses-hapus-transaksi.php?hapus=<?php echo $hasil['No_Order']; ?>" class="btn btn-danger btn-sm">Hapus</a>
           <?php } ?>
     </tr>
 
