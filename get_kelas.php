ce9d96a186daae186f5d7859e669e0a6ee057990
rizalvalry <rizal.muh.rzl@gmail.com>
2021-10-07 10:20:05 +0700

    fix

--- get_kelas.php
+++ get_kelas.php
@@ -3,11 +3,11 @@
 
 	echo "<option value=''>Pilih Kelas</option>";
 
-	$query = "SELECT * FROM kelas ORDER BY nama ASC";
-	$dewan1 = $conn->prepare($query);
-	$dewan1->execute();
-	$res1 = $dewan1->get_result();
-	while ($row = $res1->fetch_assoc()) {
+	$query = mysqli_query($conn, "SELECT * FROM kelas ORDER BY nama ASC");
+	// $dewan1 = $conn->prepare($query);
+	// $dewan1->execute();
+	// $res1 = $dewan1->get_result();
+	while ($row = mysqli_fetch_array($query)) {
 		echo "<option value='" . $row['id_kelas'] . "'>" . $row['nama'] . "</option>";
 	}
 ?>
