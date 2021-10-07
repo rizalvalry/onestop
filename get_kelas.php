5c22fbd02cdcb1ad76bb4990e7b7ec2071e47caf
rizalvalry <rizal.muh.rzl@gmail.com>
2021-10-07 10:30:00 +0700

    fix

--- get_kelas.php
+++ get_kelas.php
@@ -3,11 +3,11 @@
 
 	echo "<option value=''>Pilih Kelas</option>";
 
-	$query = mysqli_query($conn, "SELECT * FROM kelas ORDER BY nama ASC");
+	$kelas = mysqli_query($conn, "SELECT * FROM kelas ORDER BY nama ASC");
 	// $dewan1 = $conn->prepare($query);
 	// $dewan1->execute();
 	// $res1 = $dewan1->get_result();
-	while ($row = mysqli_fetch_array($query)) {
-		echo "<option value='" . $row['id_kelas'] . "'>" . $row['nama'] . "</option>";
+	while ($rowkelas = mysqli_fetch_array($kelas)) {
+		echo "<option value='" . $rowkelas['id_kelas'] . "'>" . $rowkelas['nama'] . "</option>";
 	}
 ?>
--- get_skala.php
+++ get_skala.php
@@ -4,12 +4,12 @@
 
 	echo "<option value=''>Pilih Skala</option>";
 
-	$query = mysqli_query($conn, "SELECT * FROM skala WHERE id_kelas=? ORDER BY nama ASC");
+	$skala = mysqli_query($conn, "SELECT * FROM skala WHERE id_kelas=? ORDER BY nama ASC");
 	// $dewan1 = $conn->prepare($query);
 	// $dewan1->bind_param("i", $kelas);
 	// $dewan1->execute();
 	// $res1 = $dewan1->get_result();
-	while ($row = mysqli_fetch_array($query)) {
-		echo "<option value='" . $row['id_skala'] . "'>" . $row['nama'] . "</option>";
+	while ($rowskala = mysqli_fetch_array($skala)) {
+		echo "<option value='" . $rowskala['id_skala'] . "'>" . $rowskala['nama'] . "</option>";
 	}
 ?>
